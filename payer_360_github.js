ecfdae7538f9e0141c318e271c7d7a933c8b681e
curio13 <114978560+curio13@users.noreply.github.com>
2026-04-20 12:20:12 +0530

    Added meta fields to payer_360_github.js from CSV via Lambda

--- payer_360_github.js
+++ payer_360_github.js
@@ -12,16 +12,35 @@ cube(`payer_360_github`, {
     benefit_type: {
       sql: `benefit_type`,
       type: `string`,
-      primaryKey: true
+      primaryKey: true,
+      title: `Benefit Type`,
+      description: `This field tells the benefit type by Manish`,
+      meta: {
+        owner: `Annant`,
+        use_cases: `Tells the benefit`,
+        kpi: true,
+      }
     },
 
     gpo_name: {
       sql: `gpo_name`,
-      type: `string`
-  },
+      type: `string`,
+      title: `Name of Gpo`,
+      description: `This field tells the  gpo name by Manish`,
+      meta: {
+        calculation_logic: `COALESCE(rx_lives, mx_lives) - Uses RX lives if available, otherwise falls back to MX lives`,
+      }
+    },
 
   measures: {
     count: {
-      type: `count`
-  }
+      type: `count`,
+      title: `Total Count`,
+      description: `This field tells the count of the rows`,
+      format: `number`,
+      meta: {
+        owner: `Manish`,
+        domain: `Gpo_claims`,
+      }
+    }
 });
